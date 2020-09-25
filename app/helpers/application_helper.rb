8b05931e14184d5cc119b32b81331cf85753f811
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-09-25 00:43:21 -0400

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # page title helper
+  def full_title(page_title = '')
+    base_title = "First App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
