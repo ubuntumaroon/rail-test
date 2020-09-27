3de23cee4c77c905f34e1d380aeed152d4aeb5ce
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-09-27 10:24:48 -0400

    finish test

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+
+  # authentication
+  get '/signup', to: 'users#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
