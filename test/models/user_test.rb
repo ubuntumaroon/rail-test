c8ee08db6231b70e7fa9ed9976d778bf2402fb7c
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-09-27 21:55:07 -0400

    add email test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,4 +49,11 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be UNIQUE" do 
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
