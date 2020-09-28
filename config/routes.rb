f731d1831e0b261fccf3b25330777291ed379512
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-09-28 14:52:04 -0400

    add basic login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  
   root 'static_pages#home'
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
 
   # authentication
   get '/signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "login with invalid info" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: {
+      session: {
+        email:"",
+        password: ""
+      }
+    }
+
+    assert_template 'sessions/new'
+    get root_path
+    assert flash.empty?
+  end
+end
