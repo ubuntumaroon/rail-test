f73c4e0649b9946cdc512400f824c6159065ebc3
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-10-06 23:15:17 -0400

    modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,12 @@
 
 # create a main sample user
 User.create!(name: "Example User",
-  email: "example@example.com",
-  password:              "foobar",
-  password_confirmation: "foobar",
-  admin: true)
+              email: "example@example.com",
+              password:              "foobar",
+              password_confirmation: "foobar",
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -22,5 +24,7 @@ User.create!(name: "Example User",
       email: email,
       password: password,
       password_confirmation: password
+      activated: true,
+      activated_at: Time.zone.now
     )
 end
