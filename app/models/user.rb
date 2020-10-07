adba0ddb89e4ccf14093c93f83221c0cf2e32ea7
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-10-07 15:35:26 -0400

    add reset pass method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -51,6 +51,18 @@ class User < ApplicationRecord
     update_columns(activated: true, activated_at: Time.zone.now)
     # update_attribute(:activated, true)
     # update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # reset password
+  def create_reset_digest
+    self.reset_token = User.new_token
+    self.reset_digest = User.digest(reset_token)
+    update_columns(reset_digest: reset_digest, reset_sent_at: Time.zone.now)
+  end
+
+  # send password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
