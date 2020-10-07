0910beedafca7f13e41cf198177e7d94fbaeb681
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-10-07 13:39:28 -0400

    refactor activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,18 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  # Activates an account
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+    # update_attribute(:activated, true)
+    # update_attribute(:activated_at, Time.zone.now)
+  end
+
   private
     def downcase_email
       self.email = email.downcase 
