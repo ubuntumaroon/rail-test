cbb0719fda992e679e0d634b9bb0a104628fd2ec
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-10-07 21:01:43 -0400

    refactor partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
     render html: "Hello rail!"
   end
 
+  private
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  resources :microposts,             only: [:create, :destroy]
 end
