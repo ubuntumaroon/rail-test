41a0a45141a5f840d3f9cb14262b06bfbc13a7ec
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-10-08 21:11:54 -0400

    add following

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,9 @@ class User < ApplicationRecord
 
   # Defines a proto-feed.
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?",following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
