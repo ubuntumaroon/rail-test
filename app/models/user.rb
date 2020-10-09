58041206a9e499918ec66e8757f4486b9101076f
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-10-08 20:38:40 -0400

    fix feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,7 @@ class User < ApplicationRecord
 
   # Defines a proto-feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (?) OR user_id = ?",following_ids, id)
   end
 
   def follow(other_user)
